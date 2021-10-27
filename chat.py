c443ac865be4ad27c52fb8aa7eeb8fe94615379d
rajpatel <rajkpatel10@gmail.com>
2021-10-26 21:58:49 -0500

    Login page

--- chat.py
+++ chat.py
@@ -49,6 +49,5 @@ while True:
             if tag == intent["tag"]:
                 print(f'{bot_name}: {random.choice(intent["responses"])}')
 
-
     else:
         print(f'{bot_name}: I do not understand...')
